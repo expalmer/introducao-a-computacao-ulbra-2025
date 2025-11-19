6beb744a088205785b506eea753916274a0e9f8f
Yasmim <yasmim72ces@gmail.com>
2025-11-20 09:10:30 +1000

    Minhas alteracoes

--- src/students.js
+++ src/students.js
@@ -16,8 +16,13 @@ export const students = [
   {
     id: 7,
     name: "Thalia Nascimento",
-    avatar:
-      "https://api.dicebear.com/9.x/adventurer/svg?seed=Christopher",
+    avatar: "https://api.dicebear.com/9.x/adventurer/svg?seed=Christopher",
     gradient: ["#824d11ff", "#9309efff"],
+  },
+  {
+    id: 3, // escolha um ID único que não existe ainda
+    name: "Yasmim Martins Schacherslehner",
+    avatar: "https://api.dicebear.com/9.x/fun-emoji/svg?seed=Eliza",
+    gradient: ["#EEAECA", "#94BBE9"], // escolha duas cores em hexadecimal
   },
 ];
