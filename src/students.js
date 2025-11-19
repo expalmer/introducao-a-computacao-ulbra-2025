5ca7213d0b31f7bd58690e476afb757521af457e
Palmer Oliveira <expalmer@gmail.com>
2025-11-19 18:39:40 -0300

    remove

--- src/students.js
+++ src/students.js
@@ -6,11 +6,4 @@ export const students = [
       "https://api.dicebear.com/9.x/adventurer/svg?seed=Jack&eyebrows=variant02",
     gradient: ["#667eea", "#ef0909ff"],
   },
-  {
-    id: 2,
-    name: "Mamute",
-    avatar:
-      "https://api.dicebear.com/9.x/adventurer/svg?seed=Jack&eyebrows=variant02",
-    gradient: ["#66ea85ff", "#af10b4ff"],
-  },
 ];
